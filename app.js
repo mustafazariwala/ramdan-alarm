d84281f1159d4df6573242b4f403953e5dd5e40a
Mustafa Zariwala <mustafazariwala5253@gmail.com>
2025-03-10 10:35:30 +1100

    GitInit

---
+++ app.js
@@ -0,0 +1,11 @@
+const accountSid = process.env.TWILIO_SID;
+const authToken = process.env.TWILIO_TOKEN;
+
+const client = require("twilio")(accountSid, authToken);
+
+client.calls.create({
+  url: "http://demo.twilio.com/docs/voice.xml",
+  to: "+61493159415",
+  from: "+61880040204",
+})
+.then(call => console.log(call.sid));
